becee8d6d038aa224bda55ddc7c21a9b6c266149
RicardoFs <ricardo.sette2006@gmail.com>
2026-01-07 20:06:44 -0300

    connect.py

---
+++ connect.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+
+conn = sqlite3.connect('sales.db')
+df = pd.read_sql_query("SELECT * FROM sales", conn)
+conn.close()
+
+prices_list = df['value'].tolist()
+prices_list_array = np.array(prices_list)
+print(prices_list_array)
